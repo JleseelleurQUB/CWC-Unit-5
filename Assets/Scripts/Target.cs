deaa539b758a883f5f837f04b7dc7fe00a331242
JleseelleurQUB <josephleseelleur@gmail.com>
2023-10-27 18:39:08 +0100

    Update 2
    - lesson 5.2
    
    - added UI component for score
    
    - added function to game manager for updating score and UI element, and to targets for passing their individual score values
    
    - added particle effects for target destruction

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    public GameManager gameManager;
+    public ParticleSystem explosionParticle;
 
+    public int scoreValue;
     private float xSpawnRange = 4;
     private float ySpawnPos = -2;
     private float minSpeed = 12;
@@ -15,6 +18,7 @@ public class Target : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque());
@@ -31,6 +35,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(scoreValue);
     }
 
     private void OnTriggerEnter(Collider other)
